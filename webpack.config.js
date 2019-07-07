e3953896c51af5317e5ef3ef09890dcc3c9c609a
deadcode <josephsonsjoseph@gmail.com>
2019-07-07 15:51:21 -0700

    "Added my service as a submodule and created an index.html"

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  mode: 'development',
+  entry: './sidebar-views/client/src/index.jsx',
+  module: {
+    rules: [{
+      test: /\.jsx$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
+      }
+    }]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/client/dist'
+  }
+}
