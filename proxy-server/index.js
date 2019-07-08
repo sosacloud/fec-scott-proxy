4e95d306bb503440ef491533c38472ec3e06535f
deadcode <josephsonsjoseph@gmail.com>
2019-07-07 18:43:00 -0700

    "Proxy server working with my service"

--- proxy-server/index.js
+++ proxy-server/index.js
@@ -1,6 +1,36 @@
 const express = require('express');
 const app = express();
+const fetch = require('node-fetch');
+const sidebarPort = 3002;
+const localUrl = 'http://localhost';
 const port = 2800;
 
 
-app.use(express.static('/client/dist'));
+app.use(express.static(__dirname + '/../client/dist'));
+
+// RelatedTracks
+app.get('*', (req, res) => {
+  let serviceUrl = `${localUrl}:${sidebarPort}${req.originalUrl}`;
+
+  fetch(serviceUrl, {
+      method: 'GET'
+    })
+    .then((data) => {
+      return data.json();
+    })
+    .then((jsonData) => {
+      res.status(200).json(jsonData)
+    })
+    .catch((err) => {
+      res.status(404).json(err);
+    })
+
+})
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Now listening on ${port}. Visit http://localhost:2800/`)
+});
