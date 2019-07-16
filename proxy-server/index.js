63f3040a1892d7dd6e0e33ea72e1df53f5721421
deadcode <josephsonsjoseph@gmail.com>
2019-07-16 09:46:42 -0700

    "All services now rendering on proxy"

--- proxy-server/index.js
+++ proxy-server/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const fetch = require('node-fetch');
 const app = express();
+const commentsPort = 3001;
 const sidebarPort = 3002;
 const musicPlayerPort = 3003;
 const musicPlayerEndpoint = '/api/songs';
 const sidebarEndpoints = ['/api/track', '/api/user', '/api/track/likes'];
+const commentsEndpoints = ['/comments/init', '/comments/load', '/comments/new'];
 const localUrl = 'http://localhost';
 const port = 2801;
 var servicePort;
@@ -36,8 +38,12 @@ app.get('*', (req, res) => {
   } else if (req.originalUrl.includes(musicPlayerEndpoint)) {
     servicePort = musicPlayerPort;
     serviceUrl = `${localUrl}:${servicePort}${req.originalUrl}`
-  }
 
+  } else if (commentsEndpoints.some(path => req.originalUrl.includes(path))) {
+    servicePort = commentsPort;
+    serviceUrl = `${localUrl}:${servicePort}${req.originalUrl}`
+
+  }
   fetch(serviceUrl, {
       method: 'GET'
     })
